e8637895abf4cfce12b6e958a51b22ba7dce301d
nateleavitt <nateleavitt@gmail.com>
2021-09-22 00:48:48 -0700

    Fixed pathing issue

--- lib/active-campaign-simple/exception_handler.rb
+++ lib/active-campaign-simple/exception_handler.rb
@@ -1,21 +1,19 @@
-module ActiveCampaign
-  class ExceptionHandler
+class ExceptionHandler
 
-    ERRORS = {
-      404 => NotFoundError
-    }
+  ERRORS = {
+    404 => NotFoundError
+  }
 
-    def initialize(error)
-      puts "************* #{error.inspect}"
-      error_class = ERRORS[error.status]
-      if error_class
-        raise error_class, error
-      else
-        raise ActiveCampaignAPIError(error)
-      end
+  def initialize(error)
+    puts "************* #{error.inspect}"
+    error_class = ERRORS[error.status]
+    if error_class
+      raise error_class, error
+    else
+      raise ActiveCampaignAPIError(error)
     end
-
   end
+
 end
 
 class ActiveCampaignAPIError < StandardError
