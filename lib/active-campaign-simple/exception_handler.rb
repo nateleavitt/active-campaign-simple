305cf9d345c71971dbc4cc5d12223dc34abf8e28
Nathan Leavitt <nateleavitt@gmail.com>
2023-04-12 15:06:40 -0700

    Improving error messaging

--- lib/active-campaign-simple/exception_handler.rb
+++ lib/active-campaign-simple/exception_handler.rb
@@ -2,20 +2,18 @@ require 'active-campaign-simple/exceptions'
 
 module ActiveCampaign
   class ExceptionHandler
-
     ERRORS = {
       '404 Not Found' => ActiveCampaign::NotFoundError,
       '422 Unprocessable Entity' => ActiveCampaign::UnprocessableEntityError
     }
 
     def initialize(error)
-      error_class = ERRORS[error.message]
+      error_class = ERRORS.find { |status, _| error.message.start_with?(status) }&.last
       if error_class
-        raise error_class.new(error)
+        raise error_class.new(error), "#{error.message}\nResponse body:\n#{error.response&.body}\nStack trace:\n#{error.backtrace.join("\n")}"
       else
-        raise ActiveCampaign::APIError.new(error)
+        raise ActiveCampaign::APIError.new(error), "#{error.message}\nResponse body:\n#{error.response&.body}\nStack trace:\n#{error.backtrace.join("\n")}"
       end
     end
-
   end
 end
