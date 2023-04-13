6ccf31c7d9d143cc58940e0f8f77d0122c756fbc
Nathan Leavitt <nateleavitt@gmail.com>
2023-04-13 10:47:07 -0700

    Updating gem version

--- lib/active-campaign-simple/version.rb
+++ lib/active-campaign-simple/version.rb
@@ -1,4 +1,4 @@
 module ActiveCampaign
   # The version of the gem
-  VERSION = '0.3.4'.freeze unless defined?(::ActiveCampaign::VERSION)
+  VERSION = '0.3.5'.freeze unless defined?(::ActiveCampaign::VERSION)
 end
