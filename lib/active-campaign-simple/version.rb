25269907869dec5faa07fceff0f9ecb38d189c92
Nathan Leavitt <nateleavitt@gmail.com>
2024-05-09 09:37:44 -0700

    version bump

--- lib/active-campaign-simple/version.rb
+++ lib/active-campaign-simple/version.rb
@@ -1,4 +1,4 @@
 module ActiveCampaign
   # The version of the gem
-  VERSION = '0.3.5'.freeze unless defined?(::ActiveCampaign::VERSION)
+  VERSION = '0.4.0'.freeze unless defined?(::ActiveCampaign::VERSION)
 end
